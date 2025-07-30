8a1bc1dd61666412b1b70c7d1b2e6bab32a64ec9
Riki Davidtra <rikidavidtra.2310@gmail.com>
2025-07-30 13:11:13 +0700

    Remove default callback URL from Google redirect configuration

--- config/services.php
+++ config/services.php
@@ -38,6 +38,6 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => env('GOOGLE_REDIRECT_AUTH', '/auth/google/callback'),
+        'redirect' => env('GOOGLE_REDIRECT_AUTH'),
     ],
 ];
